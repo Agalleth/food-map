bb7ecdf3980dbfc3ec163489e6b470888ff9bc3f
lauralm <mein_herz_ist_kalt@hotmail.com>
2018-02-18 21:48:28 -0600

    Haciendo elementos dinámicos

--- js/data.js
+++ js/data.js
@@ -1,92 +1,93 @@
-var data = {
-  "Villa María": {
+var data = [
+  {
     "name": "Villa María",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.475290322949!2d-99.19452723745916!3d19.435064374824208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d20200d84a1915%3A0x9b4d8cd1016858fb!2sVilla+Mar%C3%ADa!5e0!3m2!1ses-419!2smx!4v1516603303590" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "mexicana",
     "adress": "Avenida Homero 704, Polanco, Polanco IV Secc, 11550 Ciudad de México, CDMX",
+    "photo": "../assets/images/mex.jpg"
   },
-  "El cuate":{
+  {
     "name": "El cuate",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d120433.7518800615!2d-99.1562939540753!3d19.38842282858692!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xff06a6063efb3c80!2sLOS+CUATES!5e0!3m2!1ses-419!2smx!4v1516607117398" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food":"Restaurante mexicano",
     "adress": "La Coruña 302, Viaducto Piedad, 08200 Ciudad de México, CDMX",
+    "photo": "../assets/images/mex2.jpg"
   },
-  "El chapulín":{
+  {
     "name": "El chapulín",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60205.61875845433!2d-99.21002432622751!3d19.41883534027902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x86f9745eaf878b37!2sChapulin!5e0!3m2!1ses-419!2smx!4v1516607228725" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food":"Restaurante mexicano, oaxaqueño",
     "adress": "Campos Elíseos 218, Polanco, Polanco IV Secc, 11560 Ciudad de México, CDMX",
+    "photo": "../assets/images/mex3.jpg"
   },
-  "El tajín":{
+  {
     "name": "El tajín",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60226.88190646533!2d-99.20727774419625!3d19.361352054798967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x58ed42f6a99a0816!2zRWwgVGFqw61u!5e0!3m2!1ses-419!2smx!4v1516607344553" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante mexicano, alta cocina",
     "adress": "Av Miguel Ángel de Quevedo 687, San Francisco, 04320 Ciudad de México, CDMX",
+    "photo": "../assets/images/mex4.jpg"
   },
-  "La Adelita":{
+  {
     "name": "La Adelita",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60264.01435391539!2d-99.18272934905177!3d19.260571768019993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x83a46dd80fa7782c!2sCOCINA+MEXICANA+%22La+Adelita%22!5e0!3m2!1ses-419!2smx!4v1516607424068" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food":"Restaurante mexicano",
     "adress": "Viaducto Tlalpan 1115, Barrio del Niño Jesús, La Joya, 14080 Tlalpan, CDMX",
+    "photo": "../assets/images/mex5.jpg"
   },
-  "Deigo":{
+  {
     "name": "Deigo",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.795745843803!2d-99.16369628677433!3d19.377994886915282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffa40e2fd917%3A0x8231412f804286bd!2sDeigo!5e0!3m2!1ses-419!2smx!4v1516607642610" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food":"Restaurante japonés tradicional",
     "adress": "Col del Valle Centro, 03100 Ciudad de México, CDMX" ,
+    "photo": "../assets/images/jap.jpg"
+
   },
-  "Rokai":{
+  {
     "name": "Rokai",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.6214909016494!2d-99.17170958677303!3d19.428753586886494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff4af5e567d1%3A0x700a5d8f90c6d55b!2sRokai!5e0!3m2!1ses-419!2smx!4v1516607721228" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante japonés",
     "adress": "Río Ebro 87, Cuauhtémoc, 06500 Ciudad de México, CDMX",
+    "photo": "../assets/images/jap2.jpg"
   },
-  "Mikasa":{
+  {
     "name": "Mikasa",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.9762575816794!2d-99.16564628677342!3d19.413431786895178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff3eb831e3d3%3A0xd5c4dd6ef3453932!2sMikasa+Sushi!5e0!3m2!1ses-419!2smx!4v1516607782433" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food":"Restaurante japonés",
     "adress": "San Luis Potosí 173, Roma Nte., 06700 Ciudad de México, CDMX",
+    "photo": "../assets/images/jap3.jpg"
   },
-  "Mog bistro":{
+  {
     "name": "Mog bistro",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.9234980035935!2d-99.15785921693687!3d19.415711123697154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff25335c4a8d%3A0x133a09f957353d28!2sRestaurante+Mog+Bistro!5e0!3m2!1ses-419!2smx!4v1516607923009" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food":"Restaurante oriental",
-    "adress": Frontera 168, Roma Norte, Roma Nte., 06700 Coauhtemoc, CDMX,
+    "adress": "Frontera 168, Roma Norte, Roma Nte., 06700 Coauhtemoc, CDMX",
+    "photo": "../assets/images/jap4.jpg"
   },
-  "Tokyo":{
+  {
     "name": "Tokyo",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.6877606274975!2d-99.1673289867731!3d19.425892386888176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff367f58df3b%3A0xf733ebcafedc4030!2sTokyo!5e0!3m2!1ses-419!2smx!4v1516608012025" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante japonés",
     "adress": "Calle Hamburgo 134, Juárez, 06600 Ciudad de México, CDMX",
+    "photo": "../assets/images/jap5.jpg"
   },
-  "Mapo gal bi":{
+  {
     "name": "Mapo gal bi",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.7506192539367!2d-99.16897258677318!3d19.423178086889646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff3633941faf%3A0x9c0b2a8b75a1c46b!2sMapo+Gal+Bi!5e0!3m2!1ses-419!2smx!4v1516608130348" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante coreano",
     "adress": "Calle Liverpool 185, Col. Juárez, Juárez, 06600 Cuauhtémoc, CDMX",
+    "photo": "../assets/images/corea.jpg"
   },
-  "Biwon":{
+  {
     "name": "Biwon",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.7506192539367!2d-99.16897258677318!3d19.423178086889646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff360c3791d5%3A0x5510cf11ab80d19b!2sRestaurante+Biwon!5e0!3m2!1ses-419!2smx!4v1516608181612" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante coreano",
     "adress": "Florencia 20, Juárez, 06600 Ciudad de México, CDMX",
+    "photo": "../assets/images/corea2.jpg"
   },
-  "Goguinara":{
+  {
     "name": "Goguinara",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.69397407704!2d-99.16889818255619!3d19.42562410000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff34375ba87b%3A0x20ecae998bf4d51b!2sGoguinara!5e0!3m2!1ses-419!2smx!4v1516608243175" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante coreano",
     "adress": "Calle Genova 20, Zona Rosa, Juárez, 06600 Cuauhtémoc, CDMX",
+    "photo": "../assets/images/corea3.jpg"
   },
-  "O'mandu":{
+  {
     "name": "O'mandu",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.69397407704!2d-99.16889818255619!3d19.42562410000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfb4ff2f61bfe4f1a!2sO&#39;MANDU!5e0!3m2!1ses-419!2smx!4v1516608311620" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante coreano",
     "adress": "Estocolmo 18, Juárez, 06600 Ciudad de México, CDMX",
+    "photo": "../assets/images/corea4.jpg"
   },
-  "Nadefo":{
+  {
     "name": "Nadefo",
-    "map": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.7424782905296!2d-99.16903765742495!3d19.423529642599927!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x23aa6adc138fef24!2sNADEFO!5e0!3m2!1ses-419!2smx!4v1516608384590" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>,
     "food": "Restaurante coreano",
     "adress": "Calle Liverpool 183, Juárez, 06600 Ciudad de México, CDMX",
+    "photo": "../assets/images/corea5.jpg"
   }
-};
+];
--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,39 @@
+//input de materialize //Function expression function global las variables son las que puedes usar en cualquier lugar del código
+var $input = $("#icon_search")
+
+/*
 $(document).ready(function(){
    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
    $('.modal').modal();
  });
+*/
+function cargarPagina(){
+//  $('.modal').modal();
+  paintRestaurants(data);
+//  $input.keyup(restaurantFilter);
+}
+
+function paintRestaurants(array){
+  //Plantilla (template) html
+  var template = '<div class="col s6 m4">' + '<img src="__src__" class="modal-trigger" data-name="__name__" data-food="__food__" data-adress="__adress__" href="#modal1" alt="Photo food">' +
+  '</div>';
+  var finalTemplate = ""; //juntar template y agregar lo que tiene el template se manda vacío para mandarle los datos.
+  //Dando atributos a los elementos de la Plantilla
+  array.forEach(function(data){ //forEach  Es un método de los array itera en todos los elementos del arreglo.
+    var $imageFood = $("<img />"); // imagen a la que vamos a aplicar el modal
+    $imageFood.attr("src", data["photo"]);
+
+    finalTemplate += /*Se vayan agregando las cosas a reemplazar*/ template.replace('__name__', data.name)
+                                                                           .replace('__food__', data.food)
+                                                                           .replace('__adress__', data.adress)
+                                                                           .replace('__src__', data.photo);
+    //objeto por corchetes o dotnotation
+  });
+
+  $("#main_food_container").html(finalTemplate) //Agregar en contenedor el template el html es el innerHTML en jquery
+  //Reemplazando los atributos
+}
+
+
+
+$(document).ready(cargarPagina);
